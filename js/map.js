a68f43e8b3e8a6c2dac68d617c52fff81bdc5703
Jiyoung  Oh <jiyoungoh@dhcp-gs-240.redrover.cornell.edu>
2018-03-06 01:09:00 -0500

    foursquare API test

--- js/map.js
+++ js/map.js
@@ -1,117 +1,161 @@
-/**function 	mapLocation(location) {
-  var self = this;
-  self.place = ko.observable(location);
-}
-**/
-function ListViewModel(){
-  //Data
-  var self = this;
-  self.locations =[
-          {name: 'Aladdin\'s Natural Eatery', location: {lat: 42.441812, lng: -76.486502}},
-          {name: 'Herbert F. Johnson Museum of Art', location: {lat: 42.450694, lng: -76.486172}},
-          {name: 'Collegetown Bagels', location: {lat: 42.442379, lng: -76.484943}},
-          {name: 'Cornell Dairy Bar', location: {lat: 42.447145, lng: -76.470797}},
-          {name: 'FR Newman Arboretum', location: {lat: 42.451541, lng: -76.454528}},
-          {name: 'Saigon Kitchen', location: {lat: 42.439465, lng: -76.507457}}
+var locations = [
+          {id:0, name: 'Aladdin\'s Natural Eatery', location: {lat: 42.441812, lng: -76.486502}},
+          {id:1, name: 'Herbert F. Johnson Museum of Art', location: {lat: 42.450694, lng: -76.486172}},
+          {id:2, name: 'Collegetown Bagels', location: {lat: 42.442379, lng: -76.484943}},
+          {id:3, name: 'Cornell Dairy Bar', location: {lat: 42.447145, lng: -76.470797}},
+          {id:4, name: 'FR Newman Arboretum', location: {lat: 42.451541, lng: -76.454528}},
+          {id:5, name: 'Saigon Kitchen', location: {lat: 42.439465, lng: -76.507457}}
         ];
 
-  self.selectedId = ko.observable();
+function ViewModel() {
+    // Data
+    var self = this;
 
-  //Behaviors
+    self.selectedId = ko.observable();
+    self.selectedData = ko.observable();
 
-  self.displaySelected = function(location){
-    self.selectedId(location);
+    // Show inbox by default
+    self.query= ko.observable('');
+
+    // Behaviours
+    self.displaySelected = function(place) {
+      //undo click if already clicked
+      if (self.selectedId() == place.id){
+        self.selectedId(null);
+        showAllMarkers();
+        myInfowindow.close();
+      } //display selected place's marker only
+      else{ self.selectedId(place.id);
+        hideMarkers();
+        showMarker(map, place.id);
+        makeInfoWindows(markers[place.id], myInfowindow);
+      };
+    };
+
+    self.locations = ko.dependentObservable(function() {
+        return ko.utils.arrayFilter(locations, function(place) {
+          var search = self.query().toLowerCase();
+            return place.name.toLowerCase().indexOf(search) >=0;
+        });
+    });
+
+};
+
+ var markers = [];
+ var map;
+ var bounds;
+ var myInfowindow;
+
+
+function initMap() {
+
+    var ithaca = {lat: 42.441459, lng: -76.485033};
+    map = new google.maps.Map(document.getElementById('map'), {
+                  zoom: 15,
+                  center: ithaca
+                 });
+    myInfowindow = new google.maps.InfoWindow();
+    bounds = new google.maps.LatLngBounds();
+
+    for (var i = 0; i < locations.length; i++){
+          var position = locations[i].location;
+          var name = locations[i].name;
+          createMarker(position, name, i);
   };
+};
 
-/**
-  self.places = ko.observableArray([
-          new mapLocation(self.locations[0])
-  ]);
-**/
-
-}
-
-ko.applyBindings(new ListViewModel());
-
-    var map;
-
-		//blank array for listings markers.
-		var markers=[];
-
-      	function initMap() {
-        	var ithaca = {lat: 42.441459, lng: -76.485033};
-        	var map = new google.maps.Map(document.getElementById('map'), {
-         			zoom: 15,
-          			center: ithaca
-        	});
-
-        //listings of my favorite places in ithaca.
-        var locations = [
-          {title: 'Aladdin\'s Natural Eatery', location: {lat: 42.441812, lng: -76.486502}},
-          {title: 'Herbert F. Johnson Museum of Art', location: {lat: 42.450694, lng: -76.486172}},
-          {title: 'Collegetown Bagels', location: {lat: 42.442379, lng: -76.484943}},
-          {title: 'Cornell Dairy Bar', location: {lat: 42.447145, lng: -76.470797}},
-          {title: 'FR Newman Arboretum', location: {lat: 42.451541, lng: -76.454528}},
-          {title: 'Saigon Kitchen', location: {lat: 42.439465, lng: -76.507457}}
-        ];
-
-        var myInfowindow = new google.maps.InfoWindow();
-		    var bounds = new google.maps.LatLngBounds();
-
-        //place markers for locations stored in data
-        for (var i = 0; i < locations.length; i++){
-        	var position = locations[i].location;
-        	var title = locations[i].title;
-        	//create a marker for location
-        	var marker = new google.maps.Marker({
-    					 position: position,
-    					 map: map,
-    					 title: title,
-    					 animation: google.maps.Animation.DROP,
-    					 id: i,
- 						 });
-        	//push the marker to markers array
-        	markers.push(marker);
-        	//make infowindow for each markers
-        	marker.addListener('click', function(){
-        		makeInfoWindows(this, myInfowindow);
-        	});
-        	//extend the bounds for new marker
-        	bounds.extend(marker.position);
-        	map.fitBounds(bounds);
-        }
-
-        document.getElementById('show').addEventListener('click', showMarkers);
-        document.getElementById('hide').addEventListener('click', hideMarkers);
-
-        function showMarkers(){
-          var bounds = new google.maps.LatLngBounds();
-          for (var i=0; i<markers.length; i++){
-            markers[i].setMap(map);
-            bounds.extend(markers[i].position);
-          }
+function createMarker(position,name, i){
+          //create a marker for location
+          var marker = new google.maps.Marker({
+               position: position,
+               map: map,
+               name: name,
+               animation: google.maps.Animation.DROP,
+               id: i,
+             });
+          //push the marker to markers array
+          markers.push(marker);
+          //make infowindow for each markers
+          marker.addListener('click', function(){
+            makeInfoWindows(this, myInfowindow);
+          });
+          //extend the bounds for new marker
+          bounds.extend(marker.position);
           map.fitBounds(bounds);
-        }
+        };
 
-        function hideMarkers(){
+
+function hideMarkers(){
           var bounds = new google.maps.LatLngBounds();
           for (var i=0; i<markers.length; i++){
             markers[i].setMap(null);
             bounds.extend(markers[i].position);
           }
           map.fitBounds(bounds);
-        }
+        };
 
-        function makeInfoWindows(marker, infowindow){
-        	//make sure the infowindow is not open yet
-        	if (infowindow.marker != marker){
-        		infowindow.marker = marker;
-        		infowindow.setContent('<div>'+ marker.title + '</div>');
-        		infowindow.open(map,marker);
-        		//make sure marker closes on close
-        		infowindow.addListener('closeclick',function(){
-        			infowindow.setMarker = null;
-        		});
-        	}
-       }
-    };
+function showMarker(map, i){
+          //var bounds = new google.maps.LatLngBounds();
+            markers[i].setMap(map);
+            //bounds.extend(markers[i].position);
+            //map.fitBounds(bounds);
+        };
+
+
+function showAllMarkers(){
+          var bounds = new google.maps.LatLngBounds();
+          for (var i=0; i<markers.length; i++){
+            markers[i].setMap(map);
+            bounds.extend(markers[i].position);
+          }
+          map.fitBounds(bounds);
+        };
+
+        document.getElementById('show').addEventListener('click', showAllMarkers);
+        document.getElementById('hide').addEventListener('click', hideMarkers);
+
+
+
+function makeInfoWindows(marker, infowindow){
+          //make sure the infowindow is not open yet
+          if (infowindow.marker != marker){
+            infowindow.marker = marker;
+            
+            location = foursquare(marker);
+            infowindow.setContent('<div>'+ location.id + '</div>');
+            infowindow.open(map,marker);
+            //make sure marker closes on close
+            infowindow.addListener('closeclick',function(){
+              infowindow.setMarker = null;
+            });
+          }
+}
+
+// Credentials for Foursquare API 
+client_id = 'PLI5CCYKVLMH141OJGJRF1PSVAFOPOE5GKQFXXM0WTX1XOS1';
+client_secret='Q1C5LMJZ0KOQJKGKDJB4UKVC5FURTRJ1O4J3WXPZ4MXG2XW5';
+
+function foursquare(marker){
+
+  var location =[];
+
+  URL = 'https://api.foursquare.com/v2/venues/search';
+  URL =+ '?client_id=' + client_id;
+  URL =+ '&client_secret=' + client_secret;
+  URL =+ '&ll=' + marker.position[0] + ',' + marker.position[1]; 
+  URL =+ '&query=' + marker.name;
+  URL =+ '&v=20180306'
+
+
+  $.getJSON(URL).done(function(data) {
+    var results = data.response.venues[0];
+    location = [{location_id : results.id}]
+  }).fail(function() {
+    alert("Error with Foursquare API call");
+  });
+
+  return location;
+}
+
+
+ko.applyBindings(new ViewModel());
